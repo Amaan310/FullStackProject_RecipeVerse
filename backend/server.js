0a3c1e1c3a762d859f71bd31b0c7b7b9b468bc37
Amaan Haque <amaanhaq77@gmail.com>
2025-09-17 02:06:44 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,25 +4,33 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
-// database connectn
+// database connection
 const dbConnect = require('./config/dbcon');
 dbConnect();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: "https://foodverse-frontend.vercel.app/" 
+}));
+
 app.use(express.static("public"));
 
 const recipe = require("./routes/recipe");
 
-app.use("/api/users", recipe);
+app.use("/api/recipes", recipe); 
 
 app.get('/', (req, res) => {
     res.send("<h1>This is the Home Page</h1>");
 });
 
 app.listen(PORT, (err) => {
+    if (err) {
+        console.error("Error starting server:", err);
+        return;
+    }
     console.log(`Server is running on port ${PORT}`);
 });
