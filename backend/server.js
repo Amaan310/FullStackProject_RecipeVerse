6c77f1bb8908127707c16bb352398127ced51c2e
Amaan Haque <amaanhaq77@gmail.com>
2025-09-17 03:44:25 +0530

    refactor: simplify server and fix CORS

--- backend/server.js
+++ backend/server.js
@@ -10,37 +10,22 @@ const PORT = process.env.PORT || 3000;
 const dbConnect = require('./config/dbcon');
 dbConnect();
 
-// --- START: NEW ROBUST CORS CONFIGURATION ---
-
 const allowedOrigins = [
     'https://foodverse-frontend.vercel.app',
     'https://foodverse-frontend-r5f7856dm-amaans310s-projects.vercel.app'
 ];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204
-};
-
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions));
+app.use(cors({
+    origin: allowedOrigins
+}));
 
 // middleware
 app.use(express.json());
 app.use(express.static("public"));
 
-const recipe = require("./routes/recipe");
+const apiRoutes = require("./routes/recipe");
 
-app.use("/api/recipes", recipe); 
+app.use("/api", apiRoutes); 
 
 app.get('/', (req, res) => {
     res.send("<h1>This is the Home Page</h1>");
